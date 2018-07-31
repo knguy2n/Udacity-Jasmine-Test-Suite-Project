91f658598c466c3125577d2e80fdf8c04ffa1ddf
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-07-30 19:04:01 -0700

    add test suite for menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,7 +45,6 @@ $(function() {
          */
          it('article names are defined', function() {
             allFeeds.forEach(function(feed) {
-                console.log(feed);
                 expect(feed.name).not.toBeNull();
                 expect(feed.name).not.toBeUndefined();
             });
@@ -54,18 +53,46 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+$(function() {
+    const body = $('body');
+
+    describe('The menu', function(){
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('menu hidden', function() {
+            
+            expect(body.hasClass('menu-hidden')).toBe(true);
 
-         /* TODO: Write a test that ensures the menu changes
+         });
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('menu toggles', function(){
+
+            $('body').toggleClass('menu-hidden');
+            expect(body.hasClass('menu-hidden')).toBe(false);
+
+            $('body').toggleClass('menu-hidden');
+            expect(body.hasClass('menu-hidden')).toBe(true);
+
+
+
+
+
+        });
+
+
+    });
+});
+        
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
